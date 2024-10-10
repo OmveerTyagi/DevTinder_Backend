ee5106702c8f04bd9816bbafc25d24054914807c
Omveer Tyagi <omveertyagi544437@gmail.com>
2024-10-10 10:28:04 +0530

    implement and explore basic route handler

--- src/app.js
+++ src/app.js
@@ -2,55 +2,27 @@ const express = require('express');
 const app = express();
 
 
-app.get("/ab*c",(req,res)=>{
-        res.send("ABC route");
-    })
-    
-    // // app.get("/.*fly$",(req,res)=>{
-    // //     res.send("ABC route");
-    // // })
-
-app.get("/user/:id",(req ,res)=>{
-    console.log(req.params)
-    res.send("User is Found")
-    console.log("User is Found");
-    
-})
-
-app.post("/user",(req ,res)=>{
-    res.send("User Data is Send")
-    console.log("User data is Send");
-    
-})
-
-app.patch("/user",(req ,res)=>{
-    res.send("User Data is Successsfully Updated")
-    console.log("User Data is Successsfully Updated");
-    
-})
-
-app.delete("/user",(req ,res)=>{
-    res.send("User is successfully deleted")
-    console.log("User is successfully deleted");
-    
-})
-
-
-app.get("/hello/2",(req,res)=>{
-    res.send("Hello 2");
-})
-
-app.get("/hello/api/2",(req,res)=>{
-    res.send("Hello !");
-})
-
-app.get("/hello",(req,res)=>{
-    res.send("Hello  ");
-})
-
-app.use("/get",(req,res)=>{
-    res.send("Hello World!");
-})
+ app.get ("/user",[(req,res, next)=>{
+    console.log("Handle Route 1");
+    next()
+   // res.send("Response!!");
+    // next()
+ },
+(req,res, next) =>{
+    console.log("Handle Route 2");
+    next()
+    // res.send("Response  2")
+},
+(req,res, next) =>{
+    console.log("Handle Route 3");
+    next()
+    // res.send("Response  3")
+},
+(req,res, next) =>{
+    console.log("Handle Route 4");
+    res.send("Response  4")
+}]
+)
 
 app.get("/",(req,res)=>{
     res.send("Hello From The Server");
