f14f9a9794627e6d0e0cf05570650ba9d1a677bf
Omveer Tyagi <omveertyagi544437@gmail.com>
2024-10-10 10:20:07 +0530

    Explored and implemented basic routing for navigation

--- src/app.js
+++ src/app.js
@@ -1,9 +1,62 @@
 const express = require('express');
 const app = express();
 
+
+app.get("/ab*c",(req,res)=>{
+        res.send("ABC route");
+    })
+    
+    // // app.get("/.*fly$",(req,res)=>{
+    // //     res.send("ABC route");
+    // // })
+
+app.get("/user/:id",(req ,res)=>{
+    console.log(req.params)
+    res.send("User is Found")
+    console.log("User is Found");
+    
+})
+
+app.post("/user",(req ,res)=>{
+    res.send("User Data is Send")
+    console.log("User data is Send");
+    
+})
+
+app.patch("/user",(req ,res)=>{
+    res.send("User Data is Successsfully Updated")
+    console.log("User Data is Successsfully Updated");
+    
+})
+
+app.delete("/user",(req ,res)=>{
+    res.send("User is successfully deleted")
+    console.log("User is successfully deleted");
+    
+})
+
+
+app.get("/hello/2",(req,res)=>{
+    res.send("Hello 2");
+})
+
+app.get("/hello/api/2",(req,res)=>{
+    res.send("Hello !");
+})
+
+app.get("/hello",(req,res)=>{
+    res.send("Hello  ");
+})
+
+app.use("/get",(req,res)=>{
+    res.send("Hello World!");
+})
+
 app.get("/",(req,res)=>{
     res.send("Hello From The Server");
 })
+
+
 
 app.listen(5117,() =>{
     console.log("Server is running on 5117");
